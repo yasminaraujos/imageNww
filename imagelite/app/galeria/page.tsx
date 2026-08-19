9ba2c1d89484740613773751278ed3e703424792
yasminaraujos <yasmin.araujo@estudante.ifmt.edu.br>
2026-08-19 10:40:30 -0400

    Image Car

--- imagelite/app/galeria/page.tsx
+++ imagelite/app/galeria/page.tsx
@@ -1,10 +1,13 @@
 import { Template } from "../components/Template"
+import { ImageCard } from "../components/image";
 
 export default function Galeria() {
   return (
     <main>
       <Template>
-        <h1></h1>
+        <select className="grid grid-cols-3 gap-8">
+          <ImageCard/>
+        </select>
       </Template>
     </main>
   );
